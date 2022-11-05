5ed30decd75363b9f59677c60cd3851f1c05d9f3
cristian64 <cristian64@gmail.com>
2022-11-05 00:54:15 +0000

    Bumped up version from `0.1.2` to `0.1.3`.

--- mkdd_extender.py
+++ mkdd_extender.py
@@ -31,7 +31,7 @@ import rarc
 from tools import bti, gcm
 from tools.GeckoLoader import GeckoLoader
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 LANGUAGES = ('English', 'French', 'German', 'Italian', 'Japanese', 'Spanish')
 """
