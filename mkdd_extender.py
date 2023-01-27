942d491c952210058e385f406b3a4ada54d87b74
cristian64 <cristian64@gmail.com>
2023-01-27 22:30:25 +0000

    Bump up version from `0.3.0` to `0.4.0`.

--- mkdd_extender.py
+++ mkdd_extender.py
@@ -32,7 +32,7 @@ import rarc
 from tools import bti, gcm
 from tools.GeckoLoader import GeckoLoader
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 LANGUAGES = ('English', 'French', 'German', 'Italian', 'Japanese', 'Spanish')
 """
