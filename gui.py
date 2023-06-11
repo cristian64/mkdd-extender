c4bdb944104c3ad16b0108baf472bda03602c675
cristian64 <cristian64@gmail.com>
2023-06-11 19:05:56 +0100

    Add support for italics in markdown.
    Words (or text) can be surrounded by `_` to apply emphasis. (`<em>` in
    the HTML translation).

--- gui.py
+++ gui.py
@@ -2312,6 +2312,7 @@ class MKDDExtenderWindow(QtWidgets.QMainWindow):
                     paragraph = f'<ul>{unordered_list}</ul>\n'
                 else:
                     paragraph = paragraph.replace('\n', ' ')
+                paragraph = re.sub(r'\b_(.+)_\b', r'<em>\1</em>', paragraph)
                 paragraph = re.sub(r'\*\*([^\*]+)\*\*', r'<b style="white-space: nowrap;">\1</b>',
                                    paragraph)
                 paragraph = re.sub(
