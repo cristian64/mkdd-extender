71d8fb47900682bad8afcb7aba160c4bedf29d3c
cristian64 <cristian64@gmail.com>
2023-11-21 20:47:20 +0000

    Only use native file dialogs on Windows.
    With `libgdk` no longer bundled with the Linux build (see 304c52b81c),
    native file dialogs will not be available on Linux. To make this
    limitation explicit, `DontUseNativeDialog` will be set in all file
    dialogs.

--- gui.py
+++ gui.py
@@ -300,6 +300,7 @@ class PathEdit(QtWidgets.QWidget):
         name = os.path.basename(path)
         dirpath = os.path.dirname(path) or self._last_dir or os.path.expanduser('~')
         file_dialog = QtWidgets.QFileDialog(self, self._caption, dirpath)
+        file_dialog.setOption(QtWidgets.QFileDialog.DontUseNativeDialog, not mkdd_extender.windows)
         file_dialog.setAcceptMode(self._accept_mode)
         file_dialog.setFileMode(self._file_mode)
         file_dialog.setNameFilters(self._name_filters)
@@ -3084,6 +3085,8 @@ class MKDDExtenderWindow(QtWidgets.QMainWindow):
 
             file_dialog = QtWidgets.QFileDialog(self, f'Save text image as {file_type.upper()}',
                                                 dirpath)
+            file_dialog.setOption(QtWidgets.QFileDialog.DontUseNativeDialog,
+                                  not mkdd_extender.windows)
             file_dialog.setAcceptMode(QtWidgets.QFileDialog.AcceptSave)
             file_dialog.setFileMode(QtWidgets.QFileDialog.AnyFile)
             file_dialog.setNameFilters((f'{file_type.upper()} (*.{file_type})', ))
