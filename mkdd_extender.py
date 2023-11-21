4f9a01415a876de0abb9cd682d249db5064f54a4
cristian64 <cristian64@gmail.com>
2023-11-21 21:33:59 +0000

    Bump up version from `1.1.0` to `1.2.0`.

--- mkdd_extender.py
+++ mkdd_extender.py
@@ -32,7 +32,7 @@ import code_patcher
 import rarc
 from tools import bti, gcm
 
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 
 LANGUAGES = ('English', 'French', 'German', 'Italian', 'Japanese', 'Spanish')
 """
