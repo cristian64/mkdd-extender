a998e528859ca85b12f94288021c3f84dea8c96b
AlllLarryNoLurr <61329703+lance-o@users.noreply.github.com>
2024-04-26 10:35:24 +0100

    Update lib.c
    Fixed sloppy typo

--- data/code/lib.c
+++ data/code/lib.c
@@ -1059,7 +1059,7 @@ void decrement_mini_turbo_timer(char* const this)
     }
     else
     {
-        clear_boost_flag(this, 0x570, 0xfffffbff);
+        clear_boost_flag(this, 0x570, 0xfffffdff);
     }
 }
 
