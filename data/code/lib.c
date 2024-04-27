80fb755e443cb31ccc13d5d6642338d1f16306e6
lance-o <cilialawrence@yahoo.co.uk>
2024-04-27 19:48:45 +0100

    Add compiler flag for PAL to properly handle Mirror Mode X-Z inversion, so that the correct offset to find RaceManager can be found. For reference, RaceManager is usually found by looking at the address stored in r13 - 0x5c38 in NTSC-U and Japan. This offset is -0x5bd8 for Debug, and -0x5c18 in PAL. Since NTSC-U and Japan share many things, PAL and Debug are the ones that would require use of a compiler flag to patch them.

--- data/code/lib.c
+++ data/code/lib.c
@@ -19,6 +19,7 @@
 #define EXTENDER_CUP __EXTENDER_CUP__
 #define GAMEAUDIO_MAIN_ADDRESS __GAMEAUDIO_MAIN_ADDRESS__
 #define GM4E01_DEBUG_BUILD __GM4E01_DEBUG_BUILD__
+#define GM4P01_PAL __GM4P01_PAL__
 #define GP_AWARDED_SCORES_ADDRESS __GP_AWARDED_SCORES_ADDRESS__
 #define GP_COURSE_INDEX_ADDRESS __GP_COURSE_INDEX_ADDRESS__
 #define GP_CUP_INDEX_ADDRESS __GP_CUP_INDEX_ADDRESS__
@@ -42,6 +43,7 @@
 #define KART_BOUNCE_FLAG_ADRESS __KART_BOUNCE_FLAG_ADRESS__
 #define KART_BOUNCE_DEFAULT_READ_ADDRESS __KART_BOUNCE_DEFAULT_READ_ADDRESS__
 #define KART_LAST_MOMENTUM_ADRESS __KART_LAST_MOMENTUM_ADRESS__
+#define RACE_MANAGER_OFFSET_ADRESS __RACE_MANAGER_OFFSET_ADRESS__
 
 void change_course_page(const int delta)
 {
@@ -1111,7 +1113,11 @@ void handle_y_adjustment(char* const this, char* const ctrl, int kart_num)
 // Gets mirror flag from RaceManager.
 char is_mirror(char* const race_manager)
 {
+#if !GM4P01_PAL
     int* race_manager_pointer = (int*)(race_manager - 0x5c38);
+#else
+    int* race_manager_pointer = (int*)(race_manager - 0x5c18);
+#endif
     int* pointer_pointer = (int*)(*race_manager_pointer + 0x38);
     char* mirror_flag = (char*)(*pointer_pointer + 0x2c);
     return *mirror_flag;
