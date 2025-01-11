010bac67284e6e6123b60d863b5c97b9e63c123b
cristian64 <cristian64@gmail.com>
2025-01-11 12:18:17 +0000

    Bump up version from `2.2.0` to `2.2.1`.

--- mkdd_extender.py
+++ mkdd_extender.py
@@ -39,7 +39,7 @@ with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     import audioop  # Deprecated in Python 3.11.
 
-__version__ = '2.2.0'
+__version__ = '2.2.1'
 
 LANGUAGES = ('English', 'French', 'German', 'Italian', 'Japanese', 'Spanish')
 """
